58104c89a8d10779e2e3fe3f965c72b8805c6d4c
eriveltosilva <eriveltoclenio@gmail.com>
2024-05-03 14:12:30 +0100

    feat: fix requirements bugs

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# OPENAI_API_KEY = str(os.getenv('OPENAI_API_KEY'))
+OPENAI_API_KEY = str(os.getenv('OPENAI_API_KEY'))
 
 # Stylling
 st.markdown("""
@@ -22,45 +22,45 @@ div.stButton > button:hover{
 """, unsafe_allow_html=True)
 
 if 'API_KEY' not in st.session_state:
-    st.session_state['API_KEY'] =''
+    st.session_state['API_KEY'] = OPENAI_API_KEY
 
-st.title("❤️ YouTube Script Writing Tool")
+st.title("❤️ Gerador de Script para Podcast")
 
 
 # sidebar
-st.sidebar.title("😁 Get in")
-st.session_state['API_KEY'] = st.sidebar.text_input("What is your API Key?", type="password")
-btnAPI = st.sidebar.button("Adicionar")
-if btnAPI:
-    if st.session_state['API_KEY'] != '':
-        st.sidebar.write("Great to have you with us!😁💪")
-    else:
-        st.sidebar.write("Ouuu Noooo, API KEY field is empty😢😢")
-# st.sidebar.image(".Youtube.jpg", width=300, use_column_width=True)
+# st.sidebar.title("😁 Get in")
+# st.session_state['API_KEY'] = st.sidebar.text_input("What is your API Key?", type="password")
+# btnAPI = st.sidebar.button("Adicionar")
+# if btnAPI:
+#     if st.session_state['API_KEY'] != '':
+#         st.sidebar.write("Great to have you with us!😁💪")
+#     else:
+#         st.sidebar.write("Ouuu Noooo, API KEY field is empty😢😢")
+st.sidebar.image("./logo.png", width=300, use_column_width=True)
 
 # Captures user inputs
-prompt = st.text_input("Please provide the topic os the video", key="prompt")
-video_length = st.text_input("Expected video length (in minutes)", key="video_length")
-creativity = st.slider("Response Criativity Expected", 0.0, 1.0, 0.2, step=0.1)
+prompt = st.text_input("Por Favor, forneça um Tópico para o Podcast", key="prompt")
+video_length = st.text_input("Tempo estimado de video(em minutos)", key="video_length")
+creativity = st.slider("Expectativa de Criatividade da Resposta", 0.0, 1.0, 0.2, step=0.1)
 
 #
-button = st.button("Generate")
+button = st.button("Gerar")
 
 if button:
     if st.session_state['API_KEY']:
         search_result, title, script = generate_script(prompt=prompt, video_length=video_length, creativity=creativity, api_key=st.session_state['API_KEY'])
-        st.success("❤️ Hope you like and enjoy this content script ❤️")
+        st.success("❤️ Espero que goste e aproveite do conteúdo desde script ❤️")
 
-        st.subheader("Title💡")
+        st.subheader("Titulo Sugerido💡")
         st.write(title)
 
-        st.subheader("Your Video Script Content:📖📄")
+        st.subheader("Sugestão de Script:📖📄")
         st.write(script)
 
-        st.subheader("Check Out - DuckDuckGoo Search:🔍")
-        with st.expander("Show me:"):
+        st.subheader("Ver detalhes  - DuckDuckGoo Search:🔍")
+        with st.expander("Mostra-me:"):
             st.info(search_result)
     else:
-        st.error("Ooooooops!!! Please provide API KEY.....")
+        st.error("Ooooooops!!! Baixa Conexão - KEY.....")
 
         
--- utils.py
+++ utils.py
@@ -1,23 +1,23 @@
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.tools import DuckDuckGoSearchRun
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_community.tools import DuckDuckGoSearchRun
 
 def generate_script(prompt, video_length, creativity, api_key):
     # Template for generating 'Title'
     title_template = PromptTemplate(
         input_variables=['subject'],
-        template='Please come up with a title for a Youtube video on the {subject}'
+        template='Por favor, crie um título para um vídeo de Podcast no Youtube com o tópico: {subject}'
     )
 
     # Template for generating 'Video Script' using search engine
     script_template = PromptTemplate(
         input_variables=['title', 'DuckDuckGo_Search','duration'],
-        template="Create a script for a YouTube video based pn this title for me. TITLE:{title} of duration: {duration} minutes using this search data {DuckDuckGo_Search}"
+        template="Crie um roteiro para um Podcast em vídeo no YouTube baseado neste título para mim. Título:{title} de duração: {duration} minutos usando estes dados de pesquisa {DuckDuckGo_Search}"
     )
 
     #Setting up OpenAI LLM
-    llm = OpenAI(temperature=creativity, openai_api_key=api_key, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=creativity,openai_api_key=api_key,model_name='gpt-3.5-turbo-0613') 
     
     #Creating chain for 'Title' & 'Video Script'
     title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
